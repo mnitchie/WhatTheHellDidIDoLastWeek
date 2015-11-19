fe85fcc95bda38adced89ea0d140b0b14d742a39
Mike Nitchie <nitchie@careevolution.com>
2015-11-19 11:52:36 -0500

    Add routes for several tools

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('github');
+  this.route('gmail');
+  this.route('trello');
+  this.route('slack');
 });
 
 export default Router;
