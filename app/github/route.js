bd4319f410391b5b7fab49992eb7c492bdde758f
Mike Nitchie <nitchie@careevolution.com>
2015-11-19 16:27:42 -0500

    Stubbing out some lameness.

--- app/github/route.js
+++ app/github/route.js
@@ -1,9 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	session: Ember.inject.service('session'),
+
 	actions: {
-		githubLogin() {
-			alert("Woot");
-		}
+		/*githubLogin() {
+			/*this.get('session').authenticate('authenticator:torii', 'github-oauth2').then((what) => {
+				console.log(what);
+				debugger;
+			});*/
+			
+			this.get('torii').open('github-oauth2').then((authorization) => {
+				console.log(authorization);
+				Ember.$.ajax({
+					type: 'POST',
+					url: 'https://github.com/login/oauth/access_token',
+					data: {
+						client_id: '',
+						client_secret: '',
+						code: authorization.authorizationCode,
+						accept: 'application/json'
+					},
+					success: function(data, textStatus, jqXHR) {
+						debugger;
+					},
+					error: function() {
+						debugger;
+					}
+				})
+			});
+		}*/
 	}
 });
