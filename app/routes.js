4a9f09f89b19719f95bb3a08f96fbaba5171ffc8
Mike Nitchie <nitchie@careevolution.com>
2015-11-19 20:28:31 -0500

    Add key config file and set up a (kludgy) endpoint for retrieving oauth token from github

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,38 @@
+var https = require('https');
+var querystring = require('querystring');
+var keys = require('../apiKeys');
+
+var tokenRequestOptions = {
+	hostname: 'github.com',
+	path: '/login/oauth/access_token',
+	method: 'POST',
+	headers: {
+		accept: 'application/json'
+	}
+}
+
 module.exports = function(app) {
 
 	app.get('/api/', function(req, res) {
-		res.send("Go away!");
+		var tokenRequestData = querystring.stringify({
+			client_id: keys.github.clientId,
+			client_secret: keys.github.clientSecret,
+			code: req.query.code,
+		});
+
+		var tokenRequest = https.request(tokenRequestOptions, function(tokenResponse) {
+			var data = '';
+			tokenResponse.on('data', function(chunk) {
+				data += chunk;
+			});
+
+			tokenResponse.on('end', function() {
+				var returned = JSON.parse(data);
+				res.send(returned.access_token);
+			});
+		});
+		tokenRequest.write(tokenRequestData);
+		tokenRequest.end();
 	});
 
 	app.get('*', function(req, res) {
